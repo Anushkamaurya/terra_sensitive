7042cf5e9688d6f612ba82fe212683d8d6eb7034
Anushka Verma Maurya <beautiandbeast8@gmail.com>
2021-12-18 01:49:23 +0530

    remove service account credentials

--- sensitive/main.tf
+++ sensitive/main.tf
@@ -8,11 +8,6 @@ terraform {
 }
 
 
-provider "google" {
-  credentials = file("mylearning-331213-21e283592fa8.json")
-
-}
-
 resource "google_project" "my_projects" {
   name         = var.name
   project_id   = var.project_id
