788214e5b167b39d02f714682f03a752062b22bc
Anushkamaurya <94072914+Anushkamaurya@users.noreply.github.com>
2022-07-14 00:11:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ resource "google_storage_bucket" "test_bucket" {
   // org_id           = var.org_id
 } */
 
-/* resource "google_project_service" "project_services" {
+resource "google_project_service" "project_services" {
   count                      = length(var.project_services)
-  // project                    = var.project_id
+  project                    = var.project_id
   service                    = var.project_services[count.index]
   disable_on_destroy         = var.disable_services_on_destroy
   disable_dependent_services = var.disable_dependent_services
-} */
+} 
 
 /* resource "google_project_service" "project" {
   project = var.project_id
