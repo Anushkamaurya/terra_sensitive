865f6407319074f0df0bb068476da22e9636d756
Anushkamaurya <94072914+Anushkamaurya@users.noreply.github.com>
2022-07-13 22:17:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,11 +28,8 @@ resource "google_project_service" "project_services" {
   service                    = local.project_services
   disable_on_destroy         = var.disable_services_on_destroy
   disable_dependent_services = var.disable_dependent_services
-
-  depends_on = [
-    google_project_service.compute_engine
-  ]
 }
+
 /* resource "google_project_service" "project" {
   project = var.project_id
   service = "iam.googleapis.com"
