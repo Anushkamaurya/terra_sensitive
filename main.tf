3d32909797c83bba3eb3e35feca756eedbdbbc55
Anushkamaurya <94072914+Anushkamaurya@users.noreply.github.com>
2022-07-13 22:39:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,9 @@ resource "google_project" "my_project" {
 }
 
 resource "google_project_service" "project_services" {
+  count                      = length(var.project_services)
   project                    = var.project_id
-  service                    = var.project_services
+  service                    = var.project_services[count.index]
   disable_on_destroy         = var.disable_services_on_destroy
   disable_dependent_services = var.disable_dependent_services
 }
