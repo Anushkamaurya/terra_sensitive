cd99028aeb9282345d1764c7e0271e80f14859ad
Anushka Verma Maurya <beautiandbeast8@gmail.com>
2022-07-13 16:13:09 +0530

    create project

---
+++ outputs.tf
@@ -0,0 +1,31 @@
+output "project_name" {
+  value = google_project.main.name
+}
+
+output "project_id" {
+  value = module.project_services.project_id
+  depends_on = [
+    module.project_services,
+    google_project.main
+  ]
+}
+
+output "service_account_id" {
+  value       = google_service_account.service_account.account_id
+  description = "The id of the default service account"
+}
+
+output "service_account_display_name" {
+  value       = google_service_account.service_account.display_name
+  description = "The display name of the default service account"
+}
+
+output "service_account_email" {
+  value       = google_service_account.service_account.email
+  description = "The email of the default service account"
+}
+
+output "service_account_name" {
+  value       = google_service_account.service_account.name
+  description = "The fully-qualified name of the default service account"
+}
